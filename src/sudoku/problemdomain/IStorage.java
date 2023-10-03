84cce49985bf2f6adbe1243092be5be9a2e5b4d6
Chirag Arora <54665586+TheChirag356@users.noreply.github.com>
2023-10-03 18:54:33 +0530

    This is a Sudoku game made in Java. By Chirag Arora

---
+++ src/sudoku/problemdomain/IStorage.java
@@ -0,0 +1,13 @@
+package sudoku.problemdomain;
+
+import java.io.IOException;
+
+public class IStorage {
+    public void updateGameData(SudokuGame game) throws IOException {
+
+    }
+
+    public SudokuGame getGameData() throws IOException {
+        return null;
+    }
+}
